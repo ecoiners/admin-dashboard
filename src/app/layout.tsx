4bf25927944c6511d613bc0f35ceedf023270072
ecoiners <damuna63@gmail.com>
2025-09-04 01:00:45 +0700

    updated all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
-
 import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// Konfigurasi font Roboto Mono
+const robotoMono = Roboto_Mono({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: 'swap',
+  variable: '--font-roboto-mono',
 });
 
 export const metadata: Metadata = {
@@ -25,12 +22,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
+    <html lang="en" suppressHydrationWarning className={robotoMono.variable}>
+      <body className="font-mono antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full bg-background border-b border-b-gray-400 backdrop-blur-3xl">
       <div className="container flex items-center justify-end h-14">
-          <Button className="bg-pink-500 text-white">
+          <Button>
 					  Login
 					</Button>
       </div>
