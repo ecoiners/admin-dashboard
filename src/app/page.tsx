2600d662dc0b32599f2ea2d916690746cf8937e8
ecoiners <damuna63@gmail.com>
2025-09-04 02:57:04 +0700

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
 			<MetricCards metrics={metrics} />
 			
 			<div >
-			  <UsersTable users={users}/>
+			  <UsersTable data={users}/>
 			</div>
 		</main>
   );
--- src/components/users-table.tsx
+++ src/components/users-table.tsx
@@ -1,43 +1,54 @@
 import {
-	Table,
-	TableHeader,
-	TableRow,
-	TableHead,
-	TableBody,
-	TableCell
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell
 } from "@/components/ui/table";
 
-type User = {
-	id: number,
-	firtsName?: string,
-	lastName?: string,
-	lastSignInAt: number,
-	emailAddress: {id: number, emailAddress: string, [key: string] : any;}[],
-	[key: string]: any;
+// Define proper types without using 'any'
+type EmailAddress = {
+  id: number;
+  emailAddress: string;
 };
 
-export function UsersTable({data} : {data: User[]}) {
-	
-	return (
-		<Table>
-		  <TableHeader>
-			  <TableRow>
-				  <TableHead>User</TableHead>
-					<TableHead>Email</TableHead>
-					<TableHead>Last Seen</TableHead>
-				</TableRow>
-			</TableHeader>
-			<TableBody>
-			  {
-					data.map((user) => (
-						<TableRow key={user.id}>
-						  <TableCell>{user.firtsName} {user.lastName}</TableCell>
-							<TableCell>{user.emailAddress[0].emailAddress}</TableCell>
-							<TableCell>{new Date(user.lastSignInAt).toLocaleString()}</TableCell>
-						</TableRow>
-					))
-				}
-			</TableBody>
-		</Table>
-	);
+type User = {
+  id: number;
+  firstName?: string;
+  lastName?: string;
+  lastSignInAt: number;
+  emailAddress: EmailAddress[];
 };
+
+export function UsersTable({ data }: { data: User[] }) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>User</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead>Last Seen</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>
+              {user.firstName} {user.lastName}
+            </TableCell>
+            <TableCell>
+              {user.emailAddress[0]?.emailAddress || 'No email'}
+            </TableCell>
+            <TableCell>
+              {user.lastSignInAt 
+                ? new Date(user.lastSignInAt).toLocaleString() 
+                : 'Never'
+              }
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
