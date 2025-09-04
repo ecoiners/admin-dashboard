9c96bc36d9d96affb6df961ac2fd33a19e1cfeb3
ecoiners <damuna63@gmail.com>
2025-09-04 21:35:55 +0700

    src/app/page. FIXtt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import {MetricCards, type Metric} from "@/components/metric-cards";
 import {Users, DollarSign, CreditCard, Activity} from "lucide-react";
 import {UsersTable, type User} from "@/components/users-table";
-import {AddBanne} from "@/components/add-banner";
+import {AddBanner} from "@/components/add-banner";
 import {QuickLinks} from "@/components/quick-link";
 
 const metrics: Metric[] = [
